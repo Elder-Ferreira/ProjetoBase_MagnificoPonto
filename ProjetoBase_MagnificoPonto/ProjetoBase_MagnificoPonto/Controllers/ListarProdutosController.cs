c139c567d93c727a4bc2d76251b6e69af86fb5c2
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-04-13 17:47:13 -0300

    Adicionado gerenciamento do usuário

---
+++ ProjetoBase_MagnificoPonto/ProjetoBase_MagnificoPonto/Controllers/ListarProdutosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjetoBase_MagnificoPonto.Data;
+
+namespace ProjetoBase_MagnificoPonto.Controllers
+{
+    public class ListarProdutosController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+
+        public ListarProdutosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult>ListarProdutos()
+
+        {
+            var listarProdutos = await _context.Produtos.ToListAsync();
+            return View(listarProdutos);
+        }
+
+
+        // GET: Produtos/Details/5
+        public async Task<IActionResult> SaibaMais(int? id)
+        {
+            if (id == null || _context.Produtos == null)
+            {
+                return NotFound();
+            }
+
+            var produtoModel = await _context.Produtos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (produtoModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(produtoModel);
+        }
+
+
+    }
+}
--- ProjetoBase_MagnificoPonto/ProjetoBase_MagnificoPonto/obj/Debug/net6.0/ProjetoBase_MagnificoPonto.AssemblyInfo.cs
+++ ProjetoBase_MagnificoPonto/ProjetoBase_MagnificoPonto/obj/Debug/net6.0/ProjetoBase_MagnificoPonto.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoBase_MagnificoPonto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02500d06bc0407a4c30b403686ee9c3a8f1e21bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16226e9ae6247fdbc6823ad961ac294af43b8d5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoBase_MagnificoPonto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoBase_MagnificoPonto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
